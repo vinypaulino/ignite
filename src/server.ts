0657cbe0e89ccf8e4b8a62ae072f6f8b6d7e1fd5
Vinicius Paulino <vinicius.paulino@gerencianet.com.br>
2021-11-10 00:34:31 +0000

    feat: creates server

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,11 @@
-import express from 'express';
+import express, { response } from 'express';
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/heathcheck", (request, response) => {
+    return response.json({message: "OK!"});
+})
+
+app.listen(3333, () => console.log("Server is running!"));
